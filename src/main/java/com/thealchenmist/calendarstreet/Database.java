0c9a253f6435c156fb4673caa9f3069a0a7762a5
Jason Tan <10.1.19.15.14.tan@gmail.com>
2017-10-08 09:40:52 -0400

    Refine database to use coordinate doubles, and clean

--- src/main/java/com/thealchenmist/calendarstreet/Database.java
+++ src/main/java/com/thealchenmist/calendarstreet/Database.java
@@ -1,121 +1,123 @@
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import com.sothawo.mapjfx.Coordinate;
 
 public class Database {
-	
-	public static void createNewDatabase() {
-		String url = "jdbc:sqlite:";
-		
-		try(Connection conn = DriverManager.getConnection(url)){
-			if (conn!=null) {
-				DatabaseMetaData meta = conn.getMetaData();
-			}
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
-	}
-	
-	public static void connectToDatabase() {
-		Connection conn = null;
-		try {
-			String url = "jdbc:sqlite:event.db";
-			
-			conn = DriverManager.getConnection(url);
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-			createNewDatabase();
-		}finally {
-			try {
-				if(conn!=null){
-					conn.close();
-				}
-			} catch (SQLException ex) {
-				System.out.println(ex.getMessage());
-			}
-		}
-	}
-	
-	public static void createNewTable() {
-		String url = "jdbc:sqlite:event.db";
-		
-		String sql = "CREATE TABLE IF NOT EXISTS Events (\n"
-				+ "id integer PRIMARY KEY AUTOINCREMENT, \n"
-				+ "name text, \n"
-				+ "Description text, \n"
-				+ "StartTime blob, \n"
-				+ "EndTime blob, \n"
-				+ "Coordinates blob, \n"
-				+ "Address text \n"
-				+");";
-		try (Connection conn = DriverManager.getConnection(url);
-                Statement stmt = conn.createStatement()) {
+
+    static String url = "jdbc:sqlite:event.db";
+
+    public static void createNewDatabase() {
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                conn.getMetaData();
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void connectToDatabase() {
+        try (Connection conn = DriverManager.getConnection(url)) { }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+            createNewDatabase();
+        } 
+    }
+
+    public static void createNewTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS Events (\n" +
+                     "id integer PRIMARY KEY AUTOINCREMENT, \n" +
+                     "Name VARCHAR(255), \n" +
+                     "Description VARCHAR(255), \n" +
+                     "StartTime DATETIME, \n" +
+                     "EndTime DATETIME, \n" +
+                     "Latitude DOUBLE PRECISION \n" +
+                     "Longitude DOUBLE PRECISION \n" +
+                     "Address VARCHAR(255)\n" +
+                     ");";
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
             stmt.execute(sql);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-	}
-	
-	public static void insertEvent(Date startTime, Date endTime, String Name, String desc, String address, Coordinate location) {
-		String url = "jdbc:sqlite:event.db";
-		String sql = "INSERT INTO Events(name,Description,StartTime,EndTime,Coordinates,Address)"
-				+" VALUES(?,?,?,?,?,?)";
-		try (Connection conn = DriverManager.getConnection(url);
-                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+    }
+
+    public static int insertEvent(Date startTime, Date endTime, String Name, String desc, String address,
+            Coordinate location) {
+        String sql = "INSERT INTO Events(Name, Description, StartTime, EndTime, Latitude, Longitude, Address)"
+                + " VALUES(?, ?, ?, ?, ?, ?, ?)";
+        try (Connection conn = DriverManager.getConnection(url);
+             PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
             pstmt.setString(1, Name);
             pstmt.setString(2, desc);
-            pstmt.setObject(3, startTime);
-            pstmt.setObject(4, endTime);
-            pstmt.setObject(5, location);
-            pstmt.setString(6, address);
-			pstmt.executeUpdate();
+            pstmt.setDate(3, new java.sql.Date(startTime.getTime()));
+            pstmt.setDate(4, new java.sql.Date(endTime.getTime()));
+            pstmt.setDouble(5, location.getLatitude());
+            pstmt.setDouble(6, location.getLongitude());
+            pstmt.setString(7, address);
+            return pstmt.executeUpdate();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-	}
-	
-	public static void getEvent() {
-		String sql = "SELECT name, Description, StartTime, EndTime, Coordinates, Address FROM Events";
-		String url = "jdbc:sqlite:event.db";
-		try(Connection conn = DriverManager.getConnection(url);
-				Statement stmt = conn.createStatement();
-				ResultSet rs = stmt.executeQuery(sql)){
-		}catch(SQLException e) {
-			System.out.println(e.getMessage());;
-		}
-	}
+        return 0;
+    }
 
-		public static void updateEvent(int id, Date startTime, Date endTime, String name, String desc, String address, Coordinate location) {
-			String url = "jdbc:sqlite:event.db";
-			String sql = "UPDATE Events SET name=?, Description = ? ,"
-					+ "StartTime = ?, EndTime = ?,"
-					+ "Coordinates = ?,Address = ? WHERE id = ?";
-			try (Connection conn = DriverManager.getConnection(url);
-	                PreparedStatement pstmt = conn.prepareStatement(sql)) {
-				pstmt.setString(1, name);
-	            pstmt.setString(2, desc);
-	            pstmt.setObject(3, startTime);
-	            pstmt.setObject(4, endTime);
-	            pstmt.setObject(5, location);
-	            pstmt.setString(6, address);
-	            pstmt.setInt(7, id);
-				pstmt.executeUpdate();
-	        } catch (SQLException e) {
-	            System.out.println(e.getMessage());
-	        }
-		}
-		
-		public static void deleteEvent(int id) {
-			String sql = "DELETE FROM Events WHERE id = ?";
-			String url = "jdbc:sqlite:event.db";
-			try(Connection conn = DriverManager.getConnection(url);
-					PreparedStatement pstmt = conn.prepareStatement(sql)){
-				pstmt.setInt(1, id);
-				pstmt.executeUpdate();
-			}catch(SQLException e) {
-				System.out.println(e.getMessage());
-			}
-		}
-	
+    public static List<Event> getEvents() {
+        String sql = "SELECT Name, Description, StartTime, EndTime, Latitude, Longitude, Address FROM Events";
+
+        List<Event> results = new ArrayList<Event>();
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                results.add(new Event(rs.getDate("StartTime"),
+                                      rs.getDate("EndTime"),
+                                      rs.getString("Name"),
+                                      rs.getString("Description"),
+                                      rs.getString("Address"),
+                                      new Coordinate(rs.getDouble("Latitude"),
+                                                     rs.getDouble("Longitude"))));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return results;
+    }
+
+    public static void updateEvent(int id, Date startTime, Date endTime, String name, String desc, String address,
+            Coordinate location) {
+        String sql = "UPDATE Events SET Name=?, Description = ?," +
+                     "StartTime = ?, EndTime = ?," +
+                     "Latitude = ?, Longitude = ?, Address = ? WHERE id = ?";
+        try (Connection conn = DriverManager.getConnection(url);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+            pstmt.setString(2, desc);
+            pstmt.setDate(3, new java.sql.Date(startTime.getTime()));
+            pstmt.setDate(4, new java.sql.Date(endTime.getTime()));
+            pstmt.setDouble(5, location.getLatitude());
+            pstmt.setDouble(6, location.getLongitude());
+            pstmt.setString(7, address);;
+            pstmt.setInt(8, id);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void deleteEvent(int id) {
+        String sql = "DELETE FROM Events WHERE id = ?";
+        try (Connection conn = DriverManager.getConnection(url);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, id);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
