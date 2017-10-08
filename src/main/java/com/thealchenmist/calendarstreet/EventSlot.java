e90b4a172b6046e7074cbf76f51f1bf3abc389db
Jason Tan <10.1.19.15.14.tan@gmail.com>
2017-10-08 00:34:10 -0400

    Removed unnecessary import in EventSlot.java

--- src/main/java/com/thealchenmist/calendarstreet/EventSlot.java
+++ src/main/java/com/thealchenmist/calendarstreet/EventSlot.java
@@ -1,6 +1,5 @@
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 
 public class EventSlot extends GridPane {
 	private Label titleLabel,timeLabel,locationLabel;
@@ -8,7 +7,7 @@ public class EventSlot extends GridPane {
 		titleLabel = new Label(event.getName());
 		timeLabel = new Label(event.getStartTime() + " - " + event.getEndTime());
 		locationLabel = new Label(event.getAddress());
-		
+
 		this.add(titleLabel, 0, 0);
 		this.add(timeLabel, 0, 1);
 		this.add(locationLabel, 0, 2);
